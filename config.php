e9033d74724bbc10611d74c0615ebb161f7c55dc
Ott117 <33862281+Ott117@users.noreply.github.com>
2018-01-28 16:02:57 +0200

    abifunktsioonide faili kasutamine on defineeritud.

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 96hro
+ * Date: 28/01/2018
+ * Time: 16:04
+ *// kaustade ja failide konstantsed nimetused
+define('MODEL_DIR', 'model/');
+define('VIEW_DIR', 'views/');
+define('CONTROL_DIR', 'controllers/');
+define('LIB_DIR', 'lib/');
+// nõuame abifunktisoonide faili kasutamist
+require_once LIB_DIR.'utils.php';
+// nõuame vajalike failide kasutamist
+require_once MODEL_DIR.'template.php';
+require_once MODEL_DIR.'http.php';
+require_once MODEL_DIR.'linkobject.php';
+// objecktid pidevalt kasutada
+$http = new linkobject();
+
