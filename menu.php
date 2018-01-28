e54606d5bb9ff0183e55b9ad1237cf800a324762
Ott117 <33862281+Ott117@users.noreply.github.com>
2018-01-28 15:58:58 +0200

    Menüü on täidetud mitme elementidega

---
+++ menu.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 96hro
+ * Date: 28/01/2018
+ * Time: 15:58
+ */// loome menüü peamalli objekti template klassist
+
+$menuTmpl = new template('menu.menu');
+// loome menüü elemendi malli objekti
+$menuItemTmpl = new template('menu.menu_item');
+// tegutseme ühe menüü elemendiga
+// esimene
+$menuItemTmpl->set('menu_item_name', 'esimene');
+// täidame loodud elemendiga lehe menüü
+$menuItem = $menuItemTmpl->parse();
+$menuTmpl->add('menu_items', $menuItem);
+// tegutseme ühe menüü elemendiga
+// teine
+$menuItemTmpl->set('menu_item_name', 'teine');
+// täidame loodud elemendiga lehe menüü
+$menuItem = $menuItemTmpl->parse();
+$menuTmpl->add('menu_items', $menuItem);
+// koostame valmis menüü vaade
+$menu = $menuTmpl->parse();
+// ja lisame antud vaade peamalli elemendile
+// nimega {menu}
+$mainTmpl->set('menu', $menu);
