f87b028456d80a24f17396b579ebdb501cafd68a
Ott117 <33862281+Ott117@users.noreply.github.com>
2018-01-31 09:56:36 +0200

    menyyd

--- conf.php
+++ conf.php
@@ -4,17 +4,18 @@
  * User: 96hro
  * Date: 28/01/2018
  * Time: 16:04
- *// kaustade ja failide konstantsed nimetused
+ */
+//kaustade ja failide konstantsed nimetused
 define('MODEL_DIR', 'model/');
 define('VIEW_DIR', 'views/');
 define('CONTROL_DIR', 'controllers/');
 define('LIB_DIR', 'lib/');
+define('DEFAULT_CONTROL', 'default'); // vaikimis kasutatav kontroller
 // nõuame abifunktisoonide faili kasutamist
 require_once LIB_DIR.'utils.php';
 // nõuame vajalike failide kasutamist
 require_once MODEL_DIR.'template.php';
 require_once MODEL_DIR.'http.php';
 require_once MODEL_DIR.'linkobject.php';
-// objecktid pidevalt kasutada
+// loome objektid, mis oleks vaja pidevalt kasutada
 $http = new linkobject();
-
