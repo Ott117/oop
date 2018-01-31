54af4fc6ca3d73702b9fafd761d596ecaa331736
Ott117 <33862281+Ott117@users.noreply.github.com>
2018-01-31 09:55:45 +0200

    menyyd

--- menu.php
+++ menu.php
@@ -6,18 +6,27 @@
  * Time: 15:58
  */// loome menüü peamalli objekti template klassist
 
+// loome menüü peamalli objekti template klassist
 $menuTmpl = new template('menu.menu');
 // loome menüü elemendi malli objekti
 $menuItemTmpl = new template('menu.menu_item');
+// ###############################################
 // tegutseme ühe menüü elemendiga
 // esimene
 $menuItemTmpl->set('menu_item_name', 'esimene');
+// loome lingi
+$link = $http->getLink(array('control' => 'esimene'));
+$menuItemTmpl->set('link', $link);
 // täidame loodud elemendiga lehe menüü
 $menuItem = $menuItemTmpl->parse();
 $menuTmpl->add('menu_items', $menuItem);
+// ###############################################
 // tegutseme ühe menüü elemendiga
 // teine
 $menuItemTmpl->set('menu_item_name', 'teine');
+// loome lingi
+$link = $http->getLink(array('control' => 'teine'));
+$menuItemTmpl->set('link', $link);
 // täidame loodud elemendiga lehe menüü
 $menuItem = $menuItemTmpl->parse();
 $menuTmpl->add('menu_items', $menuItem);
